362273e1fe5736498d9c278238ce34ccd6ab3eba
thirdlf03 <thirdlf03@gmail.com>
2024-07-20 23:31:56 +0900

    update

--- routers/todo.py
+++ routers/todo.py
@@ -8,15 +8,20 @@ from db.models import user, todos
 
 todo_router = APIRouter()
 
-@todo_router.get("/todos")
-async def get_todos(user_id, db : AsyncSession = Depends(get_db)):
+@todo_router.get("")
+async def get_todos(github_id, db : AsyncSession = Depends(get_db)):
+
     result : Result = await db.execute(
         select(todos.Todo.context)
         .join(user.User, todos.Todo.user_id == user.User.user_id)
-        .where(todos.Todo.user_id == user_id)
+        .where(user.User.github_id == github_id)
+        .where(todos.Todo.user_id == user.User.user_id)
     )
     todo_list = result.fetchall()
 
     todo_list = [item for _ in todo_list for item in _]
 
     return todo_list
+
+# @todo_router.post("")
+# async def regist_todos(user_id, db : AsyncSession = Depends(get_db)):
