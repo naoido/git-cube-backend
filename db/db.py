7b38bcccc79177ee3a3663ba1bb6eb263bf3a094
thirdlf03 <thirdlf03@gmail.com>
2024-07-21 07:41:29 +0900

    test

--- db/db.py
+++ db/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "mysql+aiomysql://fl_db:password@localhost/fl_db"
+DB_URL = "mysql+aiomysql://root:password@mysql:3306/git_cube"
 
 async_engine = create_async_engine(DB_URL, echo=True)
 async_session = sessionmaker(autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession)
