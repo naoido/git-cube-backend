aa22f8ce91fef265fea2852b631455b99f7e9126
kenta-afk <kentarou9.60@icloud.com>
2024-07-21 08:24:11 +0900

    cubeエンドポイント

--- routers/cube.py
+++ routers/cube.py
@@ -1,8 +1,23 @@
 from fastapi import APIRouter, Depends
-from db.db import get_db
+from db.db import get_db 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.engine import Result
+from db.models import user, cube 
+from sqlalchemy import select, delete, update, null, desc, func
 
 cube_router = APIRouter()
 
-# @cube_router.get("")
+
+@cube_router.get("/cube")
+async def get_cube(user_id, cube_id):
+
+    result: Result = await db.execute(
+        select(user.User.user_id)
+        .join(user.User.user_id == cube.Collect_cube.user_id)
+        .join(cube.Collect_cube.cube_id == cube.Cube.cube_id)
+        .where(user.User.user_id == user_id)
+    )
+    
+    user_info = result.fetchone()
+    
+    return user_info
