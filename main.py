9cbdf24731427f27429f0285cff3d8a512f8def8
thirdlf03 <thirdlf03@gmail.com>
2024-07-21 04:15:56 +0900

    些細な変更

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import router
-from starlette.responses import RedirectResponse
-import httpx
 
 app = FastAPI()
 
@@ -22,33 +20,10 @@ app.add_middleware(
 
 app.include_router(router.router, prefix="/api")
 
-github_client_id = 'Ov23litv1YfmKcW4TyaT'
-github_client_secret = "ee65c1971e77279767e44b7cd503ca260e19f0c6"
 
 @app.get("/")
 def health():
     return {"status": "ok"}
-
-@app.get("/github-login")
-async def github_login():
-    return RedirectResponse(f'https://github.com/login/oauth/authorize?client_id={github_client_id}', status_code = 302)
-
-@app.get("/github-code")
-async def github_code(code: str):
-    params = {
-        'client_id' : github_client_id,
-        'client_secret' : github_client_secret,
-        'code' : code
-    }
-    headers = {'Accept' : 'application/json'}
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url='https://github.com/login/oauth/access_token', params=params, headers = headers)
-    response_json = response.json()
-    access_token = response_json['access_token']
-    async with httpx.AsyncClient() as client:
-        headers.update({'Authorization':f'Bearer {access_token}'})
-        response = await client.get('https://api.github.com/user', headers=headers)
-    return response.json()
 
 
 if __name__ == "__main__":
--- routers/router.py
+++ routers/router.py
@@ -1,10 +1,37 @@
 from fastapi import APIRouter
 
 from routers import user, cube, memo, todo
+from starlette.responses import RedirectResponse
+import httpx
+
 
 router = APIRouter()
+
+github_client_id = 'Ov23litv1YfmKcW4TyaT'
+github_client_secret = "ee65c1971e77279767e44b7cd503ca260e19f0c6"
 
 router.include_router(user.user_router, prefix="/user")
 router.include_router(cube.cube_router, prefix="/cube")
 router.include_router(memo.memo_router, prefix="/memo")
 router.include_router(todo.todo_router, prefix="/todo")
+
+@router.get("/github-login")
+async def github_login():
+    return RedirectResponse(f'https://github.com/login/oauth/authorize?client_id={github_client_id}', status_code = 302)
+
+@router.get("/github-code")
+async def github_code(code: str):
+    params = {
+        'client_id' : github_client_id,
+        'client_secret' : github_client_secret,
+        'code' : code
+    }
+    headers = {'Accept' : 'application/json'}
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url='https://github.com/login/oauth/access_token', params=params, headers = headers)
+    response_json = response.json()
+    access_token = response_json['access_token']
+    async with httpx.AsyncClient() as client:
+        headers.update({'Authorization':f'Bearer {access_token}'})
+        response = await client.get('https://api.github.com/user', headers=headers)
+    return response.json()
